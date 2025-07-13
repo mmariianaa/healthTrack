472161a52aa45b16644fba42e93c8837d480aaec
LeChatNoir578 <eyronromo@gmail.com>
2025-07-12 20:54:13 -0600

    cambios
    huhuihiu

--- src/main/java/com/mycompany/pantallapricipal/Graficas.java
+++ src/main/java/com/mycompany/pantallapricipal/Graficas.java
@@ -60,6 +60,11 @@ public class Graficas extends javax.swing.JFrame {
         jLabel1.setText("jLabel1");
 
         jButton1.setText("coca");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -84,6 +89,11 @@ public class Graficas extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
